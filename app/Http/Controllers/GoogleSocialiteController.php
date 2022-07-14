8adca8e4f2adb7c79a59187c7bcd53f9c167bdf5
munna <monirhasanmunna1@gmail.com>
2022-07-15 01:00:44 +0600

    socialite google and facebook

---
+++ app/Http/Controllers/GoogleSocialiteController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class GoogleSocialiteController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+       
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function handleCallback()
+    {
+        try {
+     
+            $user = Socialite::driver('google')->user();
+      
+            $finduser = User::where('social_id', $user->id)->first();
+      
+            if($finduser){
+      
+                Auth::login($finduser);
+     
+                return redirect('/home');
+      
+            }else{
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'social_id'=> $user->id,
+                    'avatar' => $user->avatar,
+                    'social_type'=> 'google',
+                    'password' => encrypt(Str::random(8))
+                ]);
+     
+                Auth::login($newUser);
+      
+                return redirect('/home');
+            }
+     
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}
